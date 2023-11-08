544448d1db86b239fd85b27d672a923a11833a56
kafujo <149900376+kafujo@users.noreply.github.com>
2023-11-07 19:40:04 -0800

    added pokeyellow files

---
+++ audio/headers/sfxheaders4.asm
@@ -0,0 +1,19 @@
+SFX_Surfing_Jump::
+	channel_count 1
+	channel 8, SFX_Surfing_Jump_Ch8
+
+SFX_Surfing_Flip::
+	channel_count 1
+	channel 5, SFX_Surfing_Flip_Ch5
+
+SFX_Surfing_Crash::
+	channel_count 1
+	channel 8, SFX_Surfing_Crash_Ch8
+
+SFX_Unknown_802cc::
+	channel_count 1
+	channel 5, SFX_Unknown_802cc_Ch5
+
+SFX_Surfing_Land::
+	channel_count 1
+	channel 8, SFX_Surfing_Land_Ch8
